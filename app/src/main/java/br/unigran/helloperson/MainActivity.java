650c360ec82696fbeb97c25e63bc4ad5c1c26355
Laboratorio <laboratorio@lab.unigran>
2022-09-29 21:11:52 -0400

    Versão 3.0 - Add codigo principal

--- app/src/main/java/br/unigran/helloperson/MainActivity.java
+++ app/src/main/java/br/unigran/helloperson/MainActivity.java
@@ -2,13 +2,34 @@ package br.unigran.helloperson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText textName;
+    Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textName = findViewById(R.id.textName);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                intent.putExtra("name", textName.getText().toString());
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/br/unigran/helloperson/MainActivity2.java
+++ app/src/main/java/br/unigran/helloperson/MainActivity2.java
@@ -3,12 +3,19 @@ package br.unigran.helloperson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setText("Olá " + getIntent().getStringExtra("name"));
     }
 }
